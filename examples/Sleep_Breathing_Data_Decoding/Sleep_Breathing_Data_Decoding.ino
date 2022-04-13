8ad7e43d6d57b1d398d62b80f7fcf1b5de32840c
Mengdu Li <mengdu.li@seeeed.cc>
2022-04-13 15:20:56 +0800

    init

---
+++ examples/Sleep_Breathing_Data_Decoding/Sleep_Breathing_Data_Decoding.ino
@@ -0,0 +1,26 @@
+#include <sleepbreathingradar.h>
+
+SleepBreathingRadar radar;
+
+void setup()
+{
+  Serial1.begin(9600);
+  Serial.begin(9600);
+  delay(1500);
+  Serial.println("Readly");
+}
+
+void loop()
+{
+ radar.recvRadarBytes();                       //Receive radar data and start processing
+ if (radar.newData == true) {                  //The data is received and transferred to the new list dataMsg[]
+    byte dataMsg[radar.dataLen+1] = {0x00};
+    dataMsg[0] = 0x55;                         //Add the header frame as the first element of the array
+    for (byte n = 0; n < radar.dataLen; n++)dataMsg[n+1] = radar.Msg[n];  //Frame-by-frame transfer
+    radar.newData = false;                     //A complete set of data frames is saved
+    
+    //radar.ShowData(dataMsg);                 //Serial port prints a set of received data frames
+    radar.Sleep_inf(dataMsg);                  //Sleep information output
+  }
+  
+}
---
+++ sleepbreathingradar.h
@@ -0,0 +1,75 @@
+#ifndef _RADAR_H__
+#define _RADAR_H__
+
+#define MESSAGE_HEAD 0x55       //Data frame header
+#define ACTIVE_REPORT 0x04      //Proactive reporting
+
+#define REPORT_RADAR 0x03       //Report radar information
+#define REPORT_OTHER 0x05       //Report other information
+
+#define HEARTBEAT 0x01          //Heartbeat Pack
+#define ABNOEMAL 0x02           //Abnormal Reset
+#define ENVIRONMENT 0x05        //Environment
+#define BODYSIGN 0x06           //Physical parameters
+#define CLOSE_AWAY 0x07         //Approach away
+
+#define CA_BE 0x01              //Approach away head frame
+#define CA_CLOSE 0x02           //Someone approaches
+#define CA_AWAY 0x03            //Some people stay away
+#define SOMEBODY_BE 0x01        //Motion state header frame
+#define SOMEBODY_MOVE 0x01      //Somebody move
+#define SOMEBODY_STOP 0x00      //Somebody stop
+#define NOBODY 0x00             //No one here
+
+#define SLEEP_INF 0x05          //Sleep radar data header frames
+#define BREATH 0x01             //Breathing parameters
+#define SCENARIO 0x03           //Scenario evaluation
+#define SLEEP_TIME 0x04         //Duration parameters
+#define SLEEP_QUALITY 0x05      //Sleep quality
+
+#define BREATH_RATE 0x01        //Breathing rate
+#define CHECK_SIGN 0x04         //Detection signal
+
+#define BREATH_HOLD 0x01        //Breath-holding abnormalities
+#define BREATH_NULL 0x02        //None
+#define BREATH_NORMAL 0x03      //Normal breathing
+#define BREATH_MOVE 0x04        //Movement abnormalities
+#define BREATH_RAPID 0x05       //Acute respiratory abnormalities
+
+#define CLOSE_AWAY_BED 0x07     //Getting in and out of bed judgment
+#define SLEEP_STATE 0x08        //Sleep state judgment
+
+#define AWAY_BED 0x00           //Leaving the bed
+#define CLOSE_BED 0x01          //Get into bed
+
+#define AWAKE 0x00              //sleep state:Awake
+#define LIGHT_SLEEP 0x01        //sleep state:Light sleep
+#define DEEP_SLEEP 0x02         //sleep state:deep sleep
+#define SLEEP_NULL 0x03         //no sleep state
+
+#define AWAKE_TIME 0x01         //Awake time
+#define LIGHT_SLEEP_TIME 0x02   //Light sleep time
+#define DEEP_SLEEP_TIME 0x03    //Deep sleep time
+
+#define SLEEP_SCORE 0x01        //Sleep quality score
+
+
+
+class SleepBreathingRadar{
+    private:
+        
+    public:
+        const byte MsgLen = 12;
+        byte dataLen = 12;
+        byte Msg[12];
+        boolean newData = false;
+        void recvRadarBytes();
+        void Bodysign_judgment(byte inf[], float Move_min, float Move_max);
+        void Situation_judgment(byte inf[]);
+        void Sleep_inf(byte inf[]);
+        void SleepTimeCalculate(unsigned char inf1, unsigned char inf2, unsigned char inf3, unsigned char inf4);
+        void ShowData(byte inf[]);
+        unsigned short int us_CalculateCrc16(unsigned char *lpuc_Frame, unsigned short int lus_Len);
+};
+
+#endif
